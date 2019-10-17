75f7c91f91c9aa61bae97b4373358c9208651c90
lisubin <2464009416@qq.com>
2019-10-17 22:33:36 +0800

    微信一物一码后台，啥都没有，大家一起开发

---
+++ src/main/java/com/onething/oneyard/OneyardApplication.java
@@ -0,0 +1,13 @@
+package com.onething.oneyard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneyardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneyardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onething/oneyard/OneyardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onething.oneyard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneyardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
